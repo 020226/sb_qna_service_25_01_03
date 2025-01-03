540bb708c4e218f634f9e342a131ce4ca84915f7
020226 <mmin08@naver.com>
2025-01-03 14:43:37 +0900

    @OneToMany 필드 추가

--- qna_service/src/main/java/com/sbs/qna_service/boundedContext/home/question/Question.java
+++ qna_service/src/main/java/com/sbs/qna_service/boundedContext/home/question/Question.java
@@ -1,9 +1,11 @@
 package com.sbs.qna_service.boundedContext.home.question;
 
+import com.sbs.qna_service.boundedContext.home.answer.Answer;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Entity // 스프링부트가 Question를 Entity로 본다.
@@ -17,4 +19,12 @@ public class Question {
   private String content;
   private LocalDateTime createDate; // DATETIME
 
+  // @OneToMany 자바세상에서의 편의를 위해 필드 생성
+  // 이 녀석은 실에 DB 테이블에 칼럼이 생성되지 않는다.
+  // DB는 리스트나 배열을 만들 수 없다.
+  // answerList : 만들어도 되고 만들지 않아도 된다.
+  // 다만 만들면 해당 객체(질문객체)에서 관련된 답변을 찾을 때 편하다.
+  // CascadeType.REMOVE : 질문이 삭제되면 답변도 같이 삭제된다.
+  @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+  private List<Answer> answerList;
 }
