c746ce702b92d121c186f80ff858c724fce36e12
020226 <mmin08@naver.com>
2025-01-03 15:58:05 +0900

    각 테스트케이스의 독립성 확보

--- qna_service/src/main/java/com/sbs/qna_service/boundedContext/home/question/QuestionRepository.java
+++ qna_service/src/main/java/com/sbs/qna_service/boundedContext/home/question/QuestionRepository.java
@@ -1,6 +1,9 @@
 package com.sbs.qna_service.boundedContext.home.question;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,4 +16,11 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
   Question findBySubjectAndContent(String subject, String content);
 
   List<Question> findBySubjectLike(String subject);
+
+  // 형태 암기
+  @Modifying // INSERT, UPDATE, DELETE와 같은 데이터 변경 작업에서만 사용
+  // nativeQuery = true 여야 MySQL 쿼리 사용이 가능
+  @Transactional
+  @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
+  void clearAutoIncrement();
 }
