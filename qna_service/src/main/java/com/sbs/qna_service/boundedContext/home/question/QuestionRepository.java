2c4d07d6436ef50d6c269b2c387c6066ce636c5e
020226 <mmin08@naver.com>
2025-01-03 15:29:54 +0900

    SQL SELECT 쿼리 테스트

--- qna_service/src/main/java/com/sbs/qna_service/boundedContext/home/question/QuestionRepository.java
+++ qna_service/src/main/java/com/sbs/qna_service/boundedContext/home/question/QuestionRepository.java
@@ -2,7 +2,15 @@ package com.sbs.qna_service.boundedContext.home.question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
   // interface로 만들면 @Repository가 생략되어 있음
   // JpaRepository<Question, Integer> -> JpaRepository<엔티티 클래스 이름, id 타입>
+
+  Question findBySubject(String subject);
+
+  Question findBySubjectAndContent(String subject, String content);
+
+  List<Question> findBySubjectLike(String subject);
 }
